cee983c2f721e46032bae38dd2bc94502a0a735b
Matthew Jeorrett <Matthew@wallet.services>
2019-07-18 16:52:24 +0100

    inital commit

---
+++ NBitcoinMaxArraySizeIssue/Program.cs
@@ -0,0 +1,25 @@
+﻿using NBitcoin;
+
+namespace NBitcoinMaxArraySizeIssue
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            var payloadBytes = new byte[2 * 1024 * 1024];
+
+            var nullDataTemplate = new TxNullDataTemplate(4194304);
+
+            var txOut = new TxOut()
+            {
+                Value = Money.Zero,
+                ScriptPubKey = nullDataTemplate.GenerateScriptPubKey(payloadBytes)
+            };
+
+            var tx = Transaction.Create(Network.TestNet);
+            tx.Outputs.Add(txOut);
+
+            tx.ToHex(); // this line fails
+        }
+    }
+}
